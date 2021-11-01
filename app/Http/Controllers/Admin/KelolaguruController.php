9bad589d454ae75f08aa8feea8aa411d8a1c9e54
LuckmanMS <huxlexnormagomedov@gmail.com>
2021-11-01 20:27:32 +0700

    [KELOLAGURU] bikin form edit,menjalankan edit

--- app/Http/Controllers/Admin/KelolaguruController.php
+++ app/Http/Controllers/Admin/KelolaguruController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
 
 
 class KelolaguruController extends Controller
 {
     public function index(){
-        return view ('admin.kelolaguru');
+        $guru = DB::table('guru')->get();
+        return view ('admin.kelolaguru',['guru' =>$guru]);
+    }
+
+    public function editview($id){
+        $guru = DB::table('guru')->where('id', $id)->get();
+        return view ('admin.editkelolaguru',['guru' =>$guru]);
+    }
+    public function edit(Request $request){
+        DB::table('guru')->where('id',$request->id)->update([
+            'nama' => $request->nama,
+            'mapel' => $request->mapel
+        ]);
+        return redirect('/admin/kelolaguru');
     }
 }
