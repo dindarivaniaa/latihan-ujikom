a3eefca0c7a92590782701526ff70fa279a81b43
Yunitarhy26@gmail.com <Yunitarhy26@gmail.com>
2021-11-15 10:46:29 +0700

    [tambahsiswa] roleguru

--- app/Http/Controllers/Teacher/KelolasiswaController.php
+++ app/Http/Controllers/Teacher/KelolasiswaController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Teacher;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\user;
+use Illuminate\Support\Facades\Hash;
+
 
 class KelolasiswaController extends Controller
 {
@@ -22,5 +25,20 @@ class KelolasiswaController extends Controller
             'nama' => $request->nama
         ]);
         return redirect('/teacher/kelolasiswa');
+    }
+
+     // tambah siswa
+     public function tambahsiswa(){
+        return view ('teacher.tambahsiswa');
+    }
+
+    public function store(Request $request){
+        $user= User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        $user->assignRole('student');
+        return redirect ('teacher/kelolasiswa');
     }
 }
---
+++ resources/views/teacher/tambahsiswa.blade.php
@@ -0,0 +1,89 @@
+@extends('template.main')
+
+@section('content')
+
+         <!-- Input groups -->
+         <div class="card">
+            <!-- Card header -->
+            <div class="card-header">
+              <h3 class="mb-0">Buat Login Siswa</h3>
+            </div>
+            
+            <!-- Card body -->
+            <div class="card-body">
+
+                <form method="POST" action="storesiswa">
+                    @csrf
+
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                      </div>
+                      <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Name" autofocus>
+
+                      @error('name')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                      </div>
+                      <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required placeholder="Email" autocomplete="email">
+
+                      @error('email')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                    </div>
+
+                  </div>
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative mb-4">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                      </div>
+                      <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="new-password">
+
+                      @error('password')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                    </div>
+
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                      </div>
+                      <input id="password-confirm" type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                  </div>
+
+
+                  <div class="row my-4">
+                    <div class="col-12">
+                      <div class="custom-control custom-control-alternative custom-checkbox">
+                        <input class="custom-control-input" id="customCheckRegister" type="checkbox">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary mt-4">Buat Akun</button>
+                  </div>
+                  <div class="text-center">
+                    <a href="{{url('teacher/kelolasiswa')}}" class="btn btn-danger mt-4">keluar</a>
+                  </div>
+                  
+                </form>
+            </div>
+          </div>
+
+@endsection
