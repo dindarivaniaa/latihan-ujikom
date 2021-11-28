7bc5d91e4cdd9bd337efb577c1aefa9fb2b0c7f0
ReihanZakya <rezakya41@gmail.com>
2021-11-28 04:54:58 -0800

    [sidebar] responsif

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -84,31 +84,6 @@
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
   <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
-  <!-- Extra details for Live View on GitHub Pages -->
-  <!-- Canonical SEO -->
-  <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
-  <!--  Social tags      -->
-  <meta name="keywords" content="dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, argon, argon ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit, argon dashboard">
-  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <!-- Schema.org markup for Google+ -->
-  <meta itemprop="name" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
-  <meta itemprop="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
-  <!-- Twitter Card data -->
-  <meta name="twitter:card" content="product">
-  <meta name="twitter:site" content="@creativetim">
-  <meta name="twitter:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
-  <meta name="twitter:description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta name="twitter:creator" content="@creativetim">
-  <meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
-  <!-- Open Graph data -->
-  <meta property="fb:app_id" content="655968634437471">
-  <meta property="og:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
-  <meta property="og:type" content="article" />
-  <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard/index.html" />
-  <meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
-  <meta property="og:description" content="Start your development with a Dashboard for Bootstrap 4." />
-  <meta property="og:site_name" content="Creative Tim" />
   <!-- Favicon -->
   <link rel="icon" href="{{asset('assets/img/brand/favicon.png')}}" type="image/png">
   <!-- Fonts -->
@@ -116,26 +91,9 @@
   <!-- Icons -->
   <link rel="stylesheet" href="{{asset('assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
   <link rel="stylesheet" href="{{asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
+  <!-- Page plugins -->
   <!-- Argon CSS -->
-  <link rel="stylesheet" href="{{asset('assets/css/argon.min.css')}}?v=1.2.1" type="text/css">
-  <!-- Google Tag Manager -->
-  {{-- <script>
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
-  </script> --}}
-  <!-- End Google Tag Manager -->
+  <link rel="stylesheet" href="{{asset('assets/css/argon.css?v=1.1.0')}}" type="text/css">
 </head>
 
 <body>
@@ -152,49 +110,20 @@
 @include('template.footer')
 
 
-  <!-- Argon Scripts -->
+ <!-- Argon Scripts -->
   <!-- Core -->
   <script src="{{asset('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
   <script src="{{asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/js-cookie/js.cookie.js')}}"></script>
   <script src="{{asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js')}}"></script>
   <script src="{{asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')}}"></script>
+  <!-- Optional JS -->
+  <script src="{{asset('assets/vendor/chart.js/dist/Chart.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/chart.js/dist/Chart.extension.js')}}"></script>
   <!-- Argon JS -->
-  <script src="{{asset('assets/js/argon.min.js?v=1.2.1')}}"></script>
+  <script src="{{asset('assets/js/argon.js?v=1.1.0')}}"></script>
   <!-- Demo JS - remove this in your project -->
   <script src="{{asset('assets/js/demo.min.js')}}"></script>
-  <script>
-    // Facebook Pixel Code Don't Delete
-    ! function(f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function() {
-        n.callMethod ?
-          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = !0;
-      n.version = '2.0';
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s)
-    }(window,
-      document, 'script', '//connect.facebook.net/en_US/fbevents.js');
-
-    try {
-      fbq('init', '111649226022273');
-      fbq('track', "PageView");
-
-    } catch (err) {
-      console.log('Facebook Track Error:', err);
-    }
-  </script>
-  <noscript>
-    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=111649226022273&ev=PageView&noscript=1" />
-  </noscript>
 </body>
 
 </html>
--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,12 +1,21 @@
 @hasrole('admin')
-<!-- Sidenav -->
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+<nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
       <!-- Brand -->
-      <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand" href="javascript:void(0)">
-          <img src="{{asset('assets/img/LOGO MAHAPUTRA.png')}}" class="navbar-brand-img" alt="...">
+      <div class="sidenav-header d-flex align-items-center">
+        <a class="navbar-brand" href="../../pages/dashboards/dashboard.html">
+          <img src="{{asset('assets/img/LOGO MAHAPUTRA.png')}}" style="display:block; margin:auto;" class="navbar-brand-img" alt="...">
         </a>
+        <div class="ml-auto">
+          <!-- Sidenav toggler -->
+          <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+            <div class="sidenav-toggler-inner">
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="navbar-inner">
         <!-- Collapse -->
@@ -14,7 +23,7 @@
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="{{ url('admin/home') }}">
+              <a  class="nav-link {{ url()->current() == url('admin/home') ? 'active' :''}}"  href="{{ url('admin/home') }}">
                 <i class="ni ni-tv-2 text-primary"></i>
                 <span class="nav-link-text">Dashboard Admin</span>
               </a>
@@ -57,141 +66,144 @@
             </li>
           </ul>
           <!-- Divider -->
-          <hr class="my-3">
-
 
         </div>
       </div>
     </div>
   </nav>
+
 @endhasrole
 
 @hasrole('teacher')
-<!-- Sidenav -->
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+<nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
       <!-- Brand -->
-      <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand" href="javascript:void(0)">
+      <div class="sidenav-header d-flex align-items-center">
+        <a class="navbar-brand" href="../../pages/dashboards/dashboard.html">
           <img src="{{asset('assets/img/LOGO MAHAPUTRA.png')}}" class="navbar-brand-img" alt="...">
         </a>
-      </div>
-      <div class="navbar-inner">
-        <!-- Collapse -->
-        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
-          <!-- Nav items -->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link"  href="{{ url('teacher/home') }}>
-                <i class="ni ni-tv-2 text-primary"></i>
-                <span class="nav-link-text">Dashboard Guru</span>
-              </a>
-            </li>
-            {{-- <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('teacher/profile') ? 'active' :''}}"  href="{{ url('teacher/profile') }}">
-                <i class="ni ni-single-02 text-yellow"></i>
-                <span class="nav-link-active">profil</span>
-                </a>
-            </li> --}}
-            <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('teacher/jadwal') ? 'active' :''}}"  href="{{ url('teacher/jadwal') }}">
-                <i class="ni ni-books text-orange"></i>
-                <span class="nav-link-text">Mata Pelajran</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('teacher/soal') ? 'active' :''}}" href="{{ url('teacher/soal') }}">
-                <i class="ni ni-ruler-pencil text-primary"></i>
-                <span class="nav-link-text">Soal</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/hasil') ? 'active' :''}}" href="{{ url('teacher/hasil') }}">
-                <i class="ni ni-bullet-list-67 text-default"></i>
-                <span class="nav-link-text">Hasil</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('teacher/kelola siswa') ? 'active' :''}}" href="{{ url('teacher/kelolasiswa') }}">
-                <i class="ni ni-badge text-primary"></i>
-                <span class="nav-link-text">Kelola Siswa</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('logout') }}">
-                <i class="ni ni-ui-04 text-info"></i>
-                <span class="nav-link-text">Logout</span>
-              </a>
-            </li>
-          </ul>
-          <!-- Divider -->
-          <hr class="my-3">
-
-
+        <div class="ml-auto">
+          <!-- Sidenav toggler -->
+          <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+            <div class="sidenav-toggler-inner">
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-@endhasrole
-
-@hasrole('student')
-<!-- Sidenav -->
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
-    <div class="scrollbar-inner">
-      <!-- Brand -->
-      <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand" href="javascript:void(0)">
-          <img src="{{asset('assets/img/LOGO MAHAPUTRA.png')}}" class="navbar-brand-img" alt="...">
-        </a>
-      </div>
       <div class="navbar-inner">
         <!-- Collapse -->
         <div class="collapse navbar-collapse" id="sidenav-collapse-main">
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/home') ? 'active' :''}}" href="{{ url('student/home') }}">
+              <a  class="nav-link {{ url()->current() == url('admin/home') ? 'active' :''}}"  href="{{ url('admin/home') }}">
                 <i class="ni ni-tv-2 text-primary"></i>
-                <span class="nav-link-text">Dashboard Siswa</span>
+                <span class="nav-link-text">Dashboard Admin</span>
               </a>
             </li>
-            {{-- <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/profile') ? 'active' :''}}"  href="{{ url('student/profile') }}">
-                <i class="ni ni-single-02 text-yellow"></i>
-                <span class="nav-link-active">profil</span>
-                </a>
-            </li> --}}
             <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/jadwal') ? 'active' :''}}" href="{{ url('student/jadwal') }}">
+              <a class="nav-link {{ url()->current() == url('admin/jadwal') ? 'active' :''}}"  href="{{ url('admin/jadwal') }}">
                 <i class="ni ni-books text-orange"></i>
                 <span class="nav-link-text">Mata Pelajaran</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/soal') ? 'active' :''}}" href="{{ url('student/soal') }}">
+              <a class="nav-link {{ url()->current() == url('admin/soal') ? 'active' :''}}" href="{{ url('admin/soal') }}">
                 <i class="ni ni-ruler-pencil text-primary"></i>
                 <span class="nav-link-text">Soal</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link {{ url()->current() == url('student/hasil') ? 'active' :''}}" href="{{ url('student/hasil') }}">
+              <a class="nav-link {{ url()->current() == url('admin/hasil') ? 'active' :''}}" href="{{ url('admin/hasil') }}">
                 <i class="ni ni-bullet-list-67 text-default"></i>
                 <span class="nav-link-text">Hasil</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ url('logout') }}">
+              <a class="nav-link {{ url()->current() == url('admin/kelolasiswa') ? 'active' :''}}" href="{{ url('admin/kelolasiswa') }}">
+                <i class="ni ni-badge text-primary"></i>
+                <span class="nav-link-text">Kelola Siswa</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"href="{{ url('logout') }}">
                 <i class="ni ni-ui-04 text-info"></i>
                 <span class="nav-link-text">Logout</span>
               </a>
             </li>
           </ul>
           <!-- Divider -->
-          <hr class="my-3">
-
 
         </div>
       </div>
     </div>
   </nav>
+
+@endhasrole
+
+@hasrole('student')
+<nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header d-flex align-items-center">
+        <a class="navbar-brand" href="../../pages/dashboards/dashboard.html">
+          <img src="{{asset('assets/img/LOGO MAHAPUTRA.png')}}" class="navbar-brand-img" alt="...">
+        </a>
+        <div class="ml-auto">
+          <!-- Sidenav toggler -->
+          <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+            <div class="sidenav-toggler-inner">
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a  class="nav-link {{ url()->current() == url('admin/home') ? 'active' :''}}"  href="{{ url('admin/home') }}">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Dashboard Admin</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link {{ url()->current() == url('admin/jadwal') ? 'active' :''}}"  href="{{ url('admin/jadwal') }}">
+                <i class="ni ni-books text-orange"></i>
+                <span class="nav-link-text">Mata Pelajaran</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link {{ url()->current() == url('admin/soal') ? 'active' :''}}" href="{{ url('admin/soal') }}">
+                <i class="ni ni-ruler-pencil text-primary"></i>
+                <span class="nav-link-text">Soal</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link {{ url()->current() == url('admin/hasil') ? 'active' :''}}" href="{{ url('admin/hasil') }}">
+                <i class="ni ni-bullet-list-67 text-default"></i>
+                <span class="nav-link-text">Hasil</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"href="{{ url('logout') }}">
+                <i class="ni ni-ui-04 text-info"></i>
+                <span class="nav-link-text">Logout</span>
+              </a>
+            </li>
+          </ul>
+          <!-- Divider -->
+
+        </div>
+      </div>
+    </div>
+  </nav>
+
 @endhasrole
